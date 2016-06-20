5d681adec4fd4e904f87a00780932dd6d726482c
Rob Warner <rwarner@grailbox.com>
2016-06-20 19:06:44 -0400

    Move model_test to db_test

--- model/db_test.go
+++ model/db_test.go
@@ -1,1 +1,29 @@
 package model
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jinzhu/gorm"
+)
+
+var dbPath = "./model-test"
+var db *gorm.DB
+
+func mkDB() {
+	err := os.MkdirAll(dbPath, 0700)
+	if err != nil {
+		panic(err)
+	}
+
+	db, err = InitDB(fmt.Sprintf("%s/test.db", dbPath), false)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func rmDB() {
+	if err := os.RemoveAll(dbPath); err != nil {
+		panic(err)
+	}
+}
