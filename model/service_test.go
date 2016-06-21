f9d46d9f927b03aa0dce65605050738a3a9e2fbc
Rob Warner <rob.warner@availity.com>
2016-06-21 11:29:58 -0400

    Add tests for service

--- model/service_test.go
+++ model/service_test.go
@@ -1,1 +1,18 @@
 package model
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetOrCreateServiceShouldCreateService(t *testing.T) {
+	service, err := GetOrCreateService(db, "lebron-james")
+	assert.Nil(t, err)
+	assert.NotNil(t, service)
+	assert.Equal(t, "lebron-james", service.Name)
+
+	var check Service
+	db.Where("name = ?", "lebron-james").First(&check)
+	assert.Equal(t, "lebron-james", check.Name)
+}
