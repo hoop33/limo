42d12c834deedf68eb52614eec751311376279e2
Rob Warner <rob.warner@availity.com>
2016-06-21 11:45:43 -0400

    Add test for no duplicate tags

--- model/tag_test.go
+++ model/tag_test.go
@@ -7,12 +7,31 @@ import (
 )
 
 func TestFindOrCreateTagByNameShouldCreateTag(t *testing.T) {
-	tag, _, err := FindOrCreateTagByName(db, "my-tag")
+	tag, created, err := FindOrCreateTagByName(db, "my-tag")
 	assert.Nil(t, err)
 	assert.NotNil(t, tag)
+	assert.True(t, created)
 	assert.Equal(t, "my-tag", tag.Name)
 
 	var check Tag
 	db.Where("name = ?", "my-tag").First(&check)
 	assert.Equal(t, "my-tag", check.Name)
+}
+
+func TestFindOrCreateTagShouldNotCreateDuplicateNames(t *testing.T) {
+	tag, created, err := FindOrCreateTagByName(db, "foo")
+	assert.Nil(t, err)
+	assert.NotNil(t, tag)
+	assert.True(t, created)
+	assert.Equal(t, "foo", tag.Name)
+
+	tag, created, err = FindOrCreateTagByName(db, "foo")
+	assert.Nil(t, err)
+	assert.NotNil(t, tag)
+	assert.False(t, created)
+	assert.Equal(t, "foo", tag.Name)
+
+	var tags []Tag
+	db.Where("name = ?", "foo").Find(&tags)
+	assert.Equal(t, 1, len(tags))
 }
