0e90d07bc3d2c83b1335389c0c7c8e0ea01c277f
Rob Warner <rob.warner@availity.com>
2016-06-21 11:40:28 -0400

    Add test for no duplicate services

--- model/service_test.go
+++ model/service_test.go
@@ -16,3 +16,19 @@ func TestGetOrCreateServiceShouldCreateService(t *testing.T) {
 	db.Where("name = ?", "lebron-james").First(&check)
 	assert.Equal(t, "lebron-james", check.Name)
 }
+
+func TestGetOrCreateServiceShouldNotCreateDuplicateNames(t *testing.T) {
+	service, err := GetOrCreateService(db, "foo")
+	assert.Nil(t, err)
+	assert.NotNil(t, service)
+	assert.Equal(t, "foo", service.Name)
+
+	service, err = GetOrCreateService(db, "foo")
+	assert.Nil(t, err)
+	assert.NotNil(t, service)
+	assert.Equal(t, "foo", service.Name)
+
+	var services []Service
+	db.Where("name = ?", "foo").Find(&services)
+	assert.Equal(t, 1, len(services))
+}
