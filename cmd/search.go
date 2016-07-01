a095f1753692432aa225154d407d3478c3d26ed1
Rob Warner <rwarner@grailbox.com>
2016-06-30 23:27:40 -0400

    Add first pass at search

--- cmd/search.go
+++ cmd/search.go
@@ -2,8 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
+	"github.com/blevesearch/bleve"
 	"github.com/hoop33/limo/config"
+	"github.com/hoop33/limo/model"
 	"github.com/spf13/cobra"
 )
 
@@ -15,6 +19,36 @@ var SearchCmd = &cobra.Command{
 	Long:    "Perform a full-text search on your stars",
 	Example: fmt.Sprintf("  %s search robust", config.ProgramName),
 	Run: func(cmd *cobra.Command, args []string) {
+		output := getOutput()
+
+		if len(args) == 0 {
+			output.Fatal("You must specify a search string")
+		}
+
+		index, err := getIndex()
+		fatalOnError(err)
+
+		query := bleve.NewMatchQuery(strings.Join(args, " "))
+		request := bleve.NewSearchRequest(query)
+		results, err := index.Search(request)
+		fatalOnError(err)
+
+		db, err := getDatabase()
+		fatalOnError(err)
+
+		for _, hit := range results.Hits {
+			ID, err := strconv.Atoi(hit.ID)
+			if err != nil {
+				output.Error(err.Error())
+			} else {
+				star, err := model.FindStarByID(db, ID)
+				if err != nil {
+					output.Error(err.Error())
+				} else {
+					output.StarLine(&star)
+				}
+			}
+		}
 	},
 }
 
