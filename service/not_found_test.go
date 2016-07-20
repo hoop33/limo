74259226185994025f100c1f6be20a8c1f14b074
Rob Warner <rwarner@grailbox.com>
2016-07-20 19:41:32 -0400

    Add test for NotFound Login

--- service/not_found_test.go
+++ service/not_found_test.go
@@ -1,1 +1,15 @@
 package service
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNotFoundLoginShouldReturnError(t *testing.T) {
+	nf := &NotFound{}
+	token, err := nf.Login()
+	assert.NotNil(t, err)
+	assert.Equal(t, "Service not found", err.Error())
+	assert.Equal(t, "", token)
+}
