75db787c87b48022251fc4de4f56cf1c11a9efeb
Rob Warner <rwarner@grailbox.com>
2016-07-20 20:02:28 -0400

    Add tests for service names

--- service/service_test.go
+++ service/service_test.go
@@ -1,1 +1,27 @@
 package service
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestServiceNameShouldDropPackage(t *testing.T) {
+	nf := &NotFound{}
+	name := Name(nf)
+	assert.Equal(t, "notfound", name)
+}
+
+func TestForNameShouldReturnErrorWhenNoService(t *testing.T) {
+	svc, err := ForName("foo")
+	assert.NotNil(t, err)
+	assert.Equal(t, "Service 'foo' not found", err.Error())
+	assert.Equal(t, "*service.NotFound", reflect.TypeOf(svc).String())
+}
+
+func TestForNameShouldReturnService(t *testing.T) {
+	svc, err := ForName("github")
+	assert.Nil(t, err)
+	assert.Equal(t, "*service.Github", reflect.TypeOf(svc).String())
+}
